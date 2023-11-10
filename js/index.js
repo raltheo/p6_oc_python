5630ff04dbc02dc938f0c384b3e4f4bf4f142713
raltheo <cobaltium360@gmail.com>
2023-11-10 14:35:47 +0100

    ajout modal film présentation

--- js/index.js
+++ js/index.js
@@ -3,15 +3,38 @@ async function GetResults(url){
     let results = await response.json()
     return results.results
 }
-
 async function GetOneResult(id){
     let response = await fetch("http://127.0.0.1:8000/api/v1/titles/"+ id)
     let result = await response.json()
     return result
 }
+async function getBestRated(wanted){
+    let films = []
+    let number = Math.ceil(wanted / 5);
+    for(let i = 1; i <= number; i++){
+        let response = await GetResults(`http://127.0.0.1:8000/api/v1/titles/?format=json&page=${i}&sort_by=-imdb_score`)
+        for(let a = 0; a <= response.length-1; a++){
+            if(films.length >= wanted)continue
+            films.push(response[a])
+        }
+    }
+    return films
+}
+async function getCategory(wanted, category){
+    let films = []
+    let number = Math.ceil(wanted / 5);
+    for(let i = 1; i <= number; i++){
+        let response = await GetResults(`http://127.0.0.1:8000/api/v1/titles/?format=json&page=${i}&genre=${category}&sort_by=-imdb_score`)
+        for(let a = 0; a <= response.length-1; a++){
+            if(films.length >= wanted)continue
+            films.push(response[a])
+        }
+    }
+    return films
+}
 
-async function getMostRating(){
-    let data = await GetResults('http://127.0.0.1:8000/api/v1/titles/?format=json&sort_by=-imdb_score,-votes')
+async function displayMostRating(){
+    let data = await GetResults('http://127.0.0.1:8000/api/v1/titles/?format=json&sort_by=-imdb_score')
     let film = await GetOneResult(data[0].id)
     document.querySelector(".container_most_rating").style.backgroundImage = `url(${film.image_url})`
     document.querySelector(".container_most_rating").innerHTML = (
@@ -20,8 +43,7 @@ async function getMostRating(){
             <h2 class="titre_most_rating">${film.title}</h2>
             <p class="film_genre_top1">${film.genres}</p>
             <p>${film.description}</p>
-            <button class="button_card">Lecture</button>
-            <button class="button_card">Info</button>
+            <button class="button_card card_film" id=${film.id}>Lecture</button>
         </div>
         `
     )
@@ -30,13 +52,11 @@ async function getMostRating(){
 
 function btn_scroll() {
     const scrollContainers = document.querySelectorAll(".container_cat");
-
     scrollContainers.forEach(function(scrollContainer) {
         const scrollButtonLeft = scrollContainer.querySelector(".button_scroll_left");
         const scrollButtonRight = scrollContainer.querySelector(".button_scroll_right");
 
         scrollButtonLeft.addEventListener("click", function() {
-            console.log("click");
             const container = scrollButtonLeft.nextElementSibling;
             container.scrollBy({
                 top: 0,
@@ -46,7 +66,6 @@ function btn_scroll() {
         });
 
         scrollButtonRight.addEventListener("click", function() {
-            console.log("click");
             const container = scrollButtonLeft.nextElementSibling;
             container.scrollBy({
                 top: 0,
@@ -57,99 +76,47 @@ function btn_scroll() {
     });
 }
 
-
-
-async function bestRated(){
-    let films = []
-    let data1 = await GetResults('http://127.0.0.1:8000/api/v1/titles/?format=json&sort_by=-imdb_score,-votes')
-    let data2 = await GetResults('http://127.0.0.1:8000/api/v1/titles/?format=json&page=2&sort_by=-imdb_score,-votes')
-    for(let i = 0; i < data1.length; i++){
-        if(i == 0){
-            continue
-        }
-        films.push(data1[i])
-    }
-    for(let i = 0; i < data2.length; i++){
-        if(films.length >= 7){
-            continue
-        }
-        films.push(data2[i])
-    }
+async function displayBestRated(wanted){
+    let films = await getBestRated(wanted)
     let bestRate = document.querySelector(".container_best_rate")
     bestRate.innerHTML = (
         films.map(film => (
             `
-            <div class="card_film" id="${film.id}" style="background-image: url(${film.image_url});">
+            <div class="card_film_css card_film" id="${film.id}" style="background-image: url(${film.image_url});">
             </div>
             `
         )).join('')
     )
 }
 
-async function category(cat , classname){
-    let films = []
-    let data1 = await GetResults(`http://127.0.0.1:8000/api/v1/titles/?format=json&genre=${cat}&sort_by=-imdb_score`)
-    let data2 = await GetResults(`http://127.0.0.1:8000/api/v1/titles/?format=json&page=2&genre=${cat}&sort_by=-imdb_score`)
-    for(let i = 0; i < data1.length; i++){        
-        films.push(data1[i])
-    }
-    for(let i = 0; i < data2.length; i++){
-        if(films.length >= 7){
-            continue
-        }
-        films.push(data2[i])
-    }
+async function displayCategory(cat , classname, wanted){
+    let films = await getCategory(wanted, cat)
     let container = document.querySelector(`.${classname}`)
     container.innerHTML = (
         films.map(film => (
             `
-            <div class="card_film" id="${film.id}" style="background-image: url(${film.image_url});">
+            <div class="card_film_css card_film" id="${film.id}" style="background-image: url(${film.image_url});">
             </div>
             `
         )).join('')
     )
 }
 
-function waitForElementToExist(selector) {
-    return new Promise(resolve => {
-      if (document.querySelector(selector)) {
-        return resolve(document.querySelector(selector));
-      }
-  
-      const observer = new MutationObserver(() => {
-        if (document.querySelector(selector)) {
-          resolve(document.querySelector(selector));
-          observer.disconnect();
-        }
-      });
-  
-      observer.observe(document.body, {
-        subtree: true,
-        childList: true,
-      });
-    });
-  }
-
-async function modal(){
+async function displayModal(){
     let modal = document.querySelector(".modal")
     modal.style.display = "none"
-    await getMostRating()
-    await bestRated()
-    await category("comedy", "container_cat_card1")
-    await category("thriller", "container_cat_card2")
-    await category("romance", "container_cat_card3")
-    
-    
+    await displayMostRating()
+    await displayBestRated(7)
+    await displayCategory("comedy", "container_cat_card1", 7)
+    await displayCategory("thriller", "container_cat_card2", 7)
+    await displayCategory("romance", "container_cat_card3", 7)
     let films = document.querySelectorAll(".card_film")
     films.forEach(function(film) {
         const id = film.id
         film.addEventListener("click", async function(){
-            
-            
             if (modal.style.display == "none"){
                 modal.style.display = "block"
                 let data = await GetOneResult(id)
-                console.log(data)
                 modal.innerHTML = (
                     `
                     <div class="close">x</div>
@@ -169,10 +136,13 @@ async function modal(){
                             <p>Duration : ${data.duration} min</p>
                         </div>
 
-                        <p>Actors : ${data.actors}</p>
-                        <p>Country : ${data.countries}</p>
-                        <p>Box office result : idk</p>
-                        <p>Resume : ${data.long_description} </p>
+                        <p class="modal_long_text">Actors : ${data.actors}</p>
+                        <div class="wrapped">
+                            <p>Country : ${data.countries}</p>
+                            <p>Box office result : ${data.worldwide_gross_income}</p>
+                        </div>
+                        
+                        <p class="modal_long_text">Resume : ${data.description} </p>
                         <br>
                         <button class="button_card">Lecture</button>
                     </div>
@@ -184,26 +154,11 @@ async function modal(){
             document.querySelector(".close").addEventListener("click", function(){
                 modal.style.display = "none"
             })
-            // document.addEventListener("click", function (event) {
-            //     if (event.target !== modal && event.target !== film) {
-            //         modal.style.display = "none"; // Hide the modal
-            //     }
             
-            // });
         })
         
     });
-
-    
-    
-    
-
-        
-    
 }
 
-
-
-
 btn_scroll()
-modal()
+displayModal()
